ae1f4529d663e3764c5611366b1f387a3d7c3384
vaidehi1604 <vaidehip@zignuts.com>
2023-10-17 11:54:56 +0530

    add institute type model

--- controllers/instituteType.js
+++ controllers/instituteType.js
@@ -6,48 +6,74 @@ const addInstituteType = (req, res) => {
   const { name } = req.body;
   const id = uuidv4();
 
-  client.query("SELECT to_regclass('institutetype')", (err, result) => {
-    if (err) {
-      console.error("Error checking if the table exists:", err);
-      res.status(500).send("Internal server error");
-      return;
-    }
-
-    // Check if the table exists
-    if (result.rows[0].to_regclass) {
-      // The table already exists; no need to create it
-      res.status(200).json("Institute Type Table Already Exists");
-    } else {
-      // The table doesn't exist; create it
-      client.query(
-        "CREATE TABLE instituteType (id UUID PRIMARY KEY DEFAULT uuid_generate_v4(), name VARCHAR(255))",
-        (createErr, createResult) => {
+  client.query(
+    "SELECT to_regclass('institutetype')",
+    (checkErr, checkResult) => {
+      if (checkErr) {
+        console.error("Error checking if the table exists:", checkErr);
+        res.status(500).send("Internal server error");
+      } else if (checkResult.rows[0].to_regclass) {
+        // Step 2: The table already exists; insert data into it
+        client.query(
+          queries.addInstituteType,
+          [id, name],
+          (insertErr, insertResult) => {
+            if (insertErr) {
+              console.error("Error inserting data:", insertErr);
+              res.status(500).send("Internal server error");
+            } else {
+              res.status(201).json({
+                id,
+                name,
+                message: "Institute Type Created Successfully",
+              });
+            }
+          }
+        );
+      } else {
+        // Step 3: The table doesn't exist; create it and insert data
+        client.query(queries.instituteType, (createErr, createResult) => {
           if (createErr) {
-            console.error("Error creating institute type table:", createErr);
+            console.error("Error creating institute type:", createErr);
             res.status(500).send("Internal server error");
           } else {
-            res.status(201).json("Institute Type Table Created Successfully");
+            client.query(
+              queries.addInstituteType,
+              [id, name],
+              (insertErr, insertResult) => {
+                if (insertErr) {
+                  console.error("Error inserting data:", insertErr);
+                  res.status(500).send("Internal server error");
+                } else {
+                  res.status(201).json({
+                    id,
+                    name,
+                    message: "Institute Type Created Successfully",
+                  });
+                }
+              }
+            );
           }
-        }
-      );
+        });
+      }
     }
-  });
+  );
 
-  //   client.query(queries.instituteType, (err, result) => {
-  //     if (err) {
-  //       console.error("Error creating institute type:", err);
-  //       res.status(500).send("Internal server error");
-  //     } else {
-  //       res
-  //         .status(201)
-  //         .json({ id, message: "Institute Type Created Successfully" });
-  //     }
-  //   });
+  // client.query(queries.instituteType, (err, result) => {
+  //   if (err) {
+  //     console.error("Error creating institute type:", err);
+  //     res.status(500).send("Internal server error");
+  //   } else {
+  //     res
+  //       .status(201)
+  //       .json({ id, message: "Institute Type Created Successfully" });
+  //   }
+  // });
 
-  client.query(queries.addInstituteType, [id, name], (err, result) => {
-    if (err) throw err;
-    res.status(201).json("Student Created Successfully!!");
-  });
+  // client.query(queries.addInstituteType, [id, name], (err, result) => {
+  //   if (err) throw err;
+  //   res.status(201).json("Student Created Successfully!!");
+  // });
 };
 
 module.exports = {
